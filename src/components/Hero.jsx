021bd41fcfbcfe4906a2bedb9ffeca16937f0fc5
Jennifer Lieu <jennilieu@gmail.com>
2025-06-16 21:54:57 -0400

    refactor: make Hero, TerminalCard DRY

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,11 @@
-import MailIcon from "../assets/MailIcon.svg";
-import GithubIcon from "../assets/GithubIcon.svg";
-import LinkedinIcon from "../assets/LinkedinIcon.svg";
-import FileTextIcon from "../assets/FileTextIcon.svg";
-
 import { TerminalCard } from "./TerminalCard";
 import Section from "./layout/Section";
+import IconLinks from "./IconLinks";
 
-export const Hero = () => {
+export default function Hero() {
   return (
     <Section>
-      <div className="py-16 md:py-24 flex flex-col md:flex-row items-center justify-between gap-10">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-10">
         <div className="md:w-7/12">
           <h2 className="text-lg font-mono mb-2">HELLO, WORLD! 👋</h2>
           <h1 className="text-4xl font-bold mb-2">
@@ -23,26 +19,7 @@ export const Hero = () => {
             mindset drives how I build. I want my code to be easy to navigate
             and my solutions built for both users and developers.
           </p>
-          <div className="flex space-x-4 mt-4">
-            <a
-              href="mailto:example@example.com"
-              className="text-gray-700 hover:text-gray-900">
-              <MailIcon width={20} height={20} />
-            </a>
-            <a
-              href="https://linkedin.com"
-              className="text-gray-700 hover:text-gray-900">
-              <LinkedinIcon width={20} height={20} />
-            </a>
-            <a
-              href="https://github.com"
-              className="text-gray-700 hover:text-gray-900">
-              <GithubIcon width={20} height={20} />
-            </a>
-            <a href="#resume" className="text-gray-700 hover:text-gray-900">
-              <FileTextIcon width={20} height={20} />
-            </a>
-          </div>
+          <IconLinks />
         </div>
         <div className="md:w-4/12">
           <TerminalCard />
@@ -50,4 +27,4 @@ export const Hero = () => {
       </div>
     </Section>
   );
-};
+}
