87b0ffb17bff125b6edc06fa2b678be8fabfb967
Jennifer Lieu <jennilieu@gmail.com>
2025-06-16 21:51:38 -0400

    refactor: create IconsLink to make Footer DRY

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,36 +1,14 @@
-import MailIcon from "../../assets/MailIcon.svg";
-import GithubIcon from "../../assets/GithubIcon.svg";
-import LinkedinIcon from "../../assets/LinkedinIcon.svg";
-import FileTextIcon from "../../assets/FileTextIcon.svg";
+import IconLinks from "../IconLinks";
 
-export const Footer = () => {
+export default function Footer() {
   return (
-    <footer className="py-8">
+    <footer className="py-8 border-t">
       <div className="max-w-6xl mx-auto px-4 flex flex-col items-center">
-        <div className="flex space-x-6 mb-4">
-          <a
-            href="mailto:example@example.com"
-            className="text-gray-500 hover:text-gray-700">
-            <MailIcon width={20} height={20} />
-          </a>
-          <a
-            href="https://linkedin.com"
-            className="text-gray-500 hover:text-gray-700">
-            <LinkedinIcon width={20} height={20} />
-          </a>
-          <a
-            href="https://github.com"
-            className="text-gray-500 hover:text-gray-700">
-            <GithubIcon width={20} height={20} />
-          </a>
-          <a href="#resume" className="text-gray-500 hover:text-gray-700">
-            <FileTextIcon width={20} height={20} />
-          </a>
-        </div>
+        <IconLinks />
         <p className="text-xs text-gray-500 text-center">
           Designed in Figma, built by me • using React and Tailwind CSS.
         </p>
       </div>
     </footer>
   );
-};
+}
